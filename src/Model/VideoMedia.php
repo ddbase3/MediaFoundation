9add56ac7e5a13527da81f4d3b5bfee61cf84f2e
Daniel Dahme <danieldahme@googlemail.com>
2026-03-27 21:29:36 +0000

    header added

--- src/Model/VideoMedia.php
+++ src/Model/VideoMedia.php
@@ -1,5 +1,20 @@
 <?php
 
+/***********************************************************************
+ * This file is part of MediaFoundation for BASE3 Framework.
+ *
+ * MediaFoundation extends the BASE3 framework with a unified API
+ * foundation for working with images, video, and audio.
+ * It provides shared interfaces for modular media processing.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/mediafoundation
+ * https://github.com/ddbase3/MediaFoundation
+ **********************************************************************/
+
 namespace MediaFoundation\Model;
 
 use MediaFoundation\Api\IVideoMedia;
